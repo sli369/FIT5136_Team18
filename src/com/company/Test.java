6fe66c23fe7a203eedba62330a9a2bd62cc5af83
QIXIN HE <qhee0011@student.monash.edu>
2020-06-04 00:44:46 +1000

    write Excel function developed.

--- src/com/company/Test.java
+++ src/com/company/Test.java
@@ -1,34 +1,50 @@
 package com.company;
 
 import jxl.Workbook;
+import jxl.WorkbookSettings;
 import jxl.read.biff.BiffException;
 import jxl.write.Label;
 import jxl.write.WritableSheet;
 import jxl.write.WritableWorkbook;
 import jxl.write.WriteException;
 
-import java.io.FileOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.OutputStream;
-import java.io.WriteAbortedException;
+import java.io.InputStream;
 
 public class Test {
 
     public static void main(String args[]) throws WriteException, IOException, BiffException {
-         MtmSystem mtm = new MtmSystem();
-        readAndWrite();
+        MtmSystem mtm = new MtmSystem();
+        WriteExcel();
     }
 
-    private static void readAndWrite() throws BiffException, IOException, WriteAbortedException, WriteException {
-        //File file = new File("forWritingIntoCandidate.xls");
-        Workbook workbook = Workbook.getWorkbook(new java.io.File("candidate.xls"));
-        OutputStream os = new FileOutputStream("candidate.xls");
-        WritableWorkbook writableWorkbook = Workbook.createWorkbook(os);
-        WritableSheet sheet = writableWorkbook.getSheet(0);
-        Label lable2 = new Label(2,2,"hello world!");
-        writableWorkbook.write();
+    private static void WriteExcel() {
+        String existingxlsPathName = "/Users/zoe/IdeaProjects/FIT5136_Team18/Candidate.xls";
+        //WorkbookSettings wbSettings = new WorkbookSettings();
+        //wbSettings.setLocale(new Locale("en", "EN"));
+        WritableWorkbook writebook = null;
+        InputStream in = null;
+        String UTF8_ENCODING = "UTF-8";
+        try {
+            WorkbookSettings setEncode = new WorkbookSettings();
+            setEncode.setEncoding(UTF8_ENCODING);
+            in = new FileInputStream(new File("/Users/zoe/IdeaProjects/FIT5136_Team18/Candidate.xls"));
+            Workbook existingWorkbook = Workbook.getWorkbook(in);
+            WritableWorkbook workbookCopy = Workbook.createWorkbook(new java.io.File("/Users/zoe/IdeaProjects/FIT5136_Team18/Candidate.xls"), existingWorkbook);
+            WritableSheet sheetToEdit = workbookCopy.getSheet(0);
+            sheetToEdit.addCell(new Label(4, 12, "test3"));
+            workbookCopy.write();
+            workbookCopy.close();
+            in.close();
+        } catch (IOException | WriteException | BiffException e) {
+            System.out.println(e.getMessage());
+        }
 
-        writableWorkbook.close();
 
     }
+
+
+
 }
